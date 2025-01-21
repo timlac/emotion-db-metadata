c0a49143b239b27690c1c05455101d98e1dd8dee
timlac <tim@lachmann.nu>
2025-01-21 16:50:42 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa_sentimotion_filename_parser',
-    version='0.7',
+    version='0.8',
     packages=find_packages(),
     install_requires=[
         "nexa-definitions-and-mappings @ git+https://github.com/timlac/nexa-definitions-and-mappings.git"
